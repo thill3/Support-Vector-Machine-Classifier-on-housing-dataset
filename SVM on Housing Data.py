054fd3d9ee8a8c18e717f598279d9f2f5e4f71d4
thill3 <79429184+thill3@users.noreply.github.com>
2021-02-28 19:50:15 -0600

    Update SVM on Housing Data.py

--- SVM on Housing Data.py
+++ SVM on Housing Data.py
@@ -75,3 +75,56 @@ rmse = np.sqrt(-negative_mse)
 print(rmse)
 
 #################################################################################
+#Randomized search
+
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
+from scipy.stats import expon, reciprocal
+from sklearn.svm import SVR
+
+sv_reg = SVR()
+#based on the book we will try a different grid
+#book answers suggest a small C and an exponentially scaled gamma
+param_grid2 = [
+    {'kernel': ['linear', 'rbf'],
+    'C': reciprocal(20, 200000), #use reciprocal when I might be clueless about the right range for the parameters
+    'gamma': expon(scale=1.0) #use exponential when I'm pretty sure of the range
+    }]
+
+rnd_search = RandomizedSearchCV(
+    sv_reg,
+    param_grid2,
+    n_iter=40,
+    cv=5,
+    scoring='neg_mean_squared_error', #,
+    verbose=3,
+    n_jobs = 6,
+    random_state=42
+    )
+
+#############################
+
+rnd_search.fit(housing_train_prepared, train_labels)
+
+print(rnd_search.best_params_)
+
+print(rnd_search.best_estimator_)
+
+negative_mse = rnd_search.best_score_
+rmse = np.sqrt(-negative_mse)
+print(rmse)
+
+'''
+Outcome is -
+Fitting 5 folds for each of 40 candidates, totalling 200 fits
+
+[Parallel(n_jobs=6)]: Using backend LokyBackend with 6 concurrent workers.
+[Parallel(n_jobs=6)]: Done  20 tasks      | elapsed:  1.6min
+[Parallel(n_jobs=6)]: Done 116 tasks      | elapsed:  9.7min
+[Parallel(n_jobs=6)]: Done 200 out of 200 | elapsed: 15.0min finished
+
+{'C': 157055.10989448498, 'gamma': 0.26497040005002437, 'kernel': 'rbf'}
+SVR(C=157055.10989448498, gamma=0.26497040005002437)
+55328.55083929752
+
+'''
